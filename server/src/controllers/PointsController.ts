19c80d44fa8ff2c02b974f7d74d2b1c6bcf6071f
Raphael Brito <brito.raphaelc@gmail.com>
2020-06-03 18:35:26 -0300

    creating index fuction with filter

--- server/src/controllers/PointsController.ts
+++ server/src/controllers/PointsController.ts
@@ -2,6 +2,23 @@ import knex from '../database/connection';
 import { Request, Response } from 'express';
 
 class PointsController {
+  async index(request: Request, response: Response) {
+    const { city, uf, items } = request.query;
+    const parsedItems = String(items)
+      .split(',')
+      .map((item) => Number(item.trim()));
+
+    const points = await knex('points')
+      .join('point_items', 'points.id', '=', 'point_items.point_id')
+      .whereIn('point_items.item_id', parsedItems)
+      .where('city', String(city))
+      .where('uf', String(uf))
+      .distinct()
+      .select('points.*');
+
+    return response.json(points);
+  }
+
   async show(request: Request, response: Response) {
     const { id } = request.params;
     const point = await knex('points').where('id', id).first();
@@ -56,7 +73,7 @@ class PointsController {
 
     await trx('point_items').insert(pointItems);
 
-    trx.commit();
+    await trx.commit();
     return response.json({
       id: point_id,
       ...point,
